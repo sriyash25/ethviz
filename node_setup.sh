c9ce6d9eed7d34aada19bc678d58c0a75a69a155
Aditya Salian <35491805+Aditya-Salian@users.noreply.github.com>
2023-04-13 02:20:38 -0400

    Added Port Number Flag
    Added port flag when running command so we can just pass of -p flag to specify the authrpc port to run the listen to when a node goes up. Set defaults as node1 and 8546 node and port name respectively. Can change later on.
    
    Eg run the following command in the ec2 instance: 
    
    bash node_setup.sh -n nodeX -p 8548

--- node_setup.sh
+++ node_setup.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-while getopts ":n:" opt; do
-  case $opt in
-    n)
-      node_name="$OPTARG"
+nodename="node1"
+port="8546"
+
+while getopts ":n:p:" opt; do
+  case ${opt} in
+    n ) nodename=$OPTARG
       ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
+    p ) port=$OPTARG
       ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
+    \? ) echo "Invalid option: $OPTARG" 1>&2
+         exit 1
+      ;;
+    : ) echo "Invalid option: $OPTARG requires an argument" 1>&2
+         exit 1
       ;;
   esac
 done
 
-if [ -z "$node_name" ]; then
-  echo "Usage: setup.sh -n node_name"
-  exit 1
-fi
-
-nohup geth --datadir "$node_name" init genesis.json &> /dev/null
-nohup geth --datadir "$node_name" --networkid 1234 --mine --miner.etherbase="0x5D16922C9D065226f030851521033366c58f6fCb" --authrpc.port 8547 --http.api personal,eth,nn
-et,web3,admin,txpool --http --http.addr "0.0.0.0" --rpc.enabledeprecatedpersonal &> /dev/null
+nohup geth --datadir $nodename init genesis.json &> /dev/null
+nohup geth --datadir $nodename --networkid 1234 --mine --miner.etherbase="0x5D16922C9D065226f030851521033366c58f6fCb" --authrpc.port $port --http.api personal,eth,net,web33,admin,txpool --http --http.addr "0.0.0.0" --rpc.enabledeprecatedpersonal &> /dev/null
