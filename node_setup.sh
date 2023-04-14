428b94a9807c9d3f322f898d6ab0fe7d962164be
Aditya Salian <35491805+Aditya-Salian@users.noreply.github.com>
2023-04-14 18:56:12 -0400

    Added more flags in the bash command

--- node_setup.sh
+++ node_setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 nodename="node1"
-port="8546"
+rpcport="8547"
+httpport="8008"
+ethport="30304"
 
-while getopts ":n:p:" opt; do
+while getopts ":n:r:h:e:" opt; do
   case ${opt} in
     n ) nodename=$OPTARG
       ;;
-    p ) port=$OPTARG
+    r ) rpcport=$OPTARG
+      ;;
+    h ) httpport=$OPTARG
+      ;;
+    e ) ethport=$OPTARG
       ;;
     \? ) echo "Invalid option: $OPTARG" 1>&2
          exit 1
@@ -19,4 +25,4 @@ while getopts ":n:p:" opt; do
 done
 
 nohup geth --datadir $nodename init genesis.json &> /dev/null
-nohup geth --datadir $nodename --networkid 1234 --mine --miner.etherbase="0x5D16922C9D065226f030851521033366c58f6fCb" --authrpc.port $port --http.api personal,eth,net,web33,admin,txpool --http --http.addr "0.0.0.0" --rpc.enabledeprecatedpersonal &> /dev/null
+nohup geth --datadir $nodename --networkid 1234 --mine --port $ethport --miner.etherbase="0x5D16922C9D065226f030851521033366c58f6fCb" --authrpc.port $rpcport --http.api personal,eth,net,web3,admin,txpool --http --http.addr "0.0.0.0" --http.port $httpport &> /dev/null
